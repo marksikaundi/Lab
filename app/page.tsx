2ef5f314225c7bb5f8f26bfcef3f2fafd67ff034
marksikaundi <emmanuelmark117@gmail.com>
2024-06-17 01:32:50 +0200

    setups

--- app/page.tsx
+++ app/page.tsx
@@ -2,10 +2,8 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main>
-      Hey, welcome to the Lupleg Challenge Dashboard! 🚀
-      <button className="btn btn-active btn-primary">Get Started</button>
-
-    </main>
+    <div>
+      Hello, world
+    </div>
   );
 }
