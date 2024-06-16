ea030b43d5f709c247963969218400a2a705ddd6
marksikaundi <emmanuelmark117@gmail.com>
2024-06-17 01:53:14 +0200

    chore: Update font family in globals.css and layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,5 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { Inter, Poppins } from "next/font/google";
 import "./globals.css";
 import {
   ClerkProvider,
@@ -10,6 +10,12 @@ import {
 } from "@clerk/nextjs";
 
 const inter = Inter({ subsets: ["latin"] });
+
+const poppins = Poppins({
+  subsets: ["latin"],
+  variable: "--font-poppins",
+  weight: ["400", "200", "300", "400", "500", "600", "700", "800", "900"],
+});
 
 export const metadata: Metadata = {
   title: "Lupleg Challenge Dashboard",
@@ -25,7 +31,7 @@ export default function RootLayout({
   return (
     <ClerkProvider>
       <html lang="en">
-        <body className={inter.className}>{children}</body>
+        <body className={poppins.className}>{children}</body>
       </html>
     </ClerkProvider>
   );
--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div>
-      Hello, world
+      <h2 className="poppins font-bold">Challenge Board</h2>
     </div>
   );
 }
