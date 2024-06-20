7fd9df4a1fc151f5390690a1c014f3d3c4f9b1cf
marksikaundi <emmanuelmark117@gmail.com>
2024-06-20 09:18:54 +0200

    updated the  Sitemaps

--- app/sitemap.js
+++ app/sitemap.js
@@ -1,40 +1,46 @@
 export default function Sitemap() {
   return [
     {
-      url: "https://www.lupleg.website/mentorships",
+      url: "https://www.lupleg.website/data-structures",
       lastModified: new Date(),
       changeFrequency: "yearly",
       priority: 1,
     },
     {
-      url: "https://www.lupleg.website/challenge",
+      url: "https://www.lupleg.website/algorithms",
       lastModified: new Date(),
       changeFrequency: "monthly",
       priority: 1.5,
     },
     {
-      url: "https://www.lupleg.website/podcasts",
+      url: "https://www.lupleg.website/coding-interviews",
       lastModified: new Date(),
       changeFrequency: "monthly",
       priority: 2,
     },
     {
-      url: "https://www.lupleg.website/academy",
+      url: "https://www.lupleg.website/frontend-practice",
       lastModified: new Date(),
       changeFrequency: "weekly",
       priority: 2.5,
     },
     {
-      url: "https://www.lupleg.website/affiliates",
+      url: "https://www.lupleg.website/backend-practice",
       lastModified: new Date(),
       changeFrequency: "weekly",
       priority: 3,
     },
     {
-      url: "https://www.lupleg.website/sponsor",
+      url: "https://www.lupleg.website/mentorships",
       lastModified: new Date(),
       changeFrequency: "weekly",
       priority: 3.5,
+    },
+    {
+      url: "https://www.lupleg.website/academy",
+      lastModified: new Date(),
+      changeFrequency: "weekly",
+      priority: 4,
     },
   ];
 }
