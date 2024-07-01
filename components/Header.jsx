c28e7ef2bad86cb1d88d93793ad95c4d25794027
marksikaundi <emmanuelmark117@gmail.com>
2024-07-02 00:39:11 +0200

    chore: Add beta tag to Header component
    This commit adds a beta tag to the Header component, which will be displayed on the website while it is in the beta phase. The tag is wrapped in a div with a background color, rounded corners, and a border. It improves the visual presentation of the website and provides a clear indication of its beta status.

--- components/Header.jsx
+++ components/Header.jsx
@@ -15,6 +15,16 @@ export default function Header() {
           <Image src="/lupleg.png" alt="Lupleg" width={300} height={200} />
         </Link>
       </div>
+      
+// TODO: Add a beta tag, but remove it when the site is out of beta
+      <div className="flex items-center space-x-3 bg-[#0F172A] rounded-md border border-white">
+        <Link href="#">
+          <h1 className="p-2 text-md font-semibold  text-white dark:text-white">
+            Beta
+          </h1>
+        </Link>
+      </div>
+
       <div className="hidden md:block">
         <nav className="flex space-x-6">
           <Link
