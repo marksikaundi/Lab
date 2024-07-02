038227ac3571f161ba30f2cc8c5a9f9f127759af
marksikaundi <emmanuelmark117@gmail.com>
2024-07-02 22:55:20 +0200

    refactor: Update middleware.js to protect routes
    This commit updates the `middleware.js` file to protect certain routes using the `clerkMiddleware` function. The `isProtectedRoute` variable is used to define the routes that require authentication. If a request matches one of these routes, the `auth().protect()` function is called to protect the route. This enhancement improves the security of the application by ensuring that only authenticated users can access protected routes.

--- middleware.js
+++ middleware.js
@@ -1,15 +1,20 @@
-import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server'
+import {
+  clerkMiddleware,
+  createRouteMatcher
+} from '@clerk/nextjs/server';
 
-const isPublicRoute = createRouteMatcher(['/sign-in(.*)', '/sign-up(.*)']);
+const isProtectedRoute = createRouteMatcher([
+  '/dashboard(.*)',
+  '/roadmap(.*)',
+  '/profile(.*)',
+  '/account-profile(.*)',
+  '/challenges(.*)',
+]);
 
-export default clerkMiddleware((auth, request) => {
-  if(!isPublicRoute(request)) {
-    auth().protect();
-  }
+export default clerkMiddleware((auth, req) => {
+  if (isProtectedRoute(req)) auth().protect();
 });
 
 export const config = {
   matcher: ['/((?!.*\\..*|_next).*)', '/', '/(api|trpc)(.*)'],
 };
-
-
