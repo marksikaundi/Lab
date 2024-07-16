86dc00d9d21b3e16f5758c1f759f42324fb642ea
marksikaundi <emmanuelmark117@gmail.com>
2024-07-16 02:18:36 +0200

    refactor: Update button styling and link in course page

--- app/(shortcourses)/web-development/page.jsx
+++ app/(shortcourses)/web-development/page.jsx
@@ -23,7 +23,7 @@ export default function ReactBasics() {
               <div className="flex flex-col gap-2 min-[400px]:flex-row">
                 <Link
                   href="#"
-                  className="inline-flex h-10 items-center justify-center rounded-md bg-primary px-8 text-sm font-medium text-primary-foreground shadow transition-colors hover:bg-primary/90 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50"
+                  className="inline-flex h-10 items-center justify-center rounded-md bg-green-950 hover:green-950 px-8 text-sm font-medium text-primary-foreground shadow transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50"
                   prefetch={false}
                 >
                   Enroll Now
