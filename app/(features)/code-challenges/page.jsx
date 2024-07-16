4e6bdc0ef0d1bb9316f2c1c490a9a3328b587f93
marksikaundi <emmanuelmark117@gmail.com>
2024-07-16 02:57:47 +0200

    formatted the code

--- app/(features)/code-challenges/page.jsx
+++ app/(features)/code-challenges/page.jsx
@@ -4,25 +4,24 @@ import Sidebar from "./sideBar";
 import { Button } from "@/components/ui/button";
 import { ImSpinner9 } from "react-icons/im";
 
-
 const Dashboard = () => {
   const [challenges, setChallenges] = useState([]);
   const [selectedChallenge, setSelectedChallenge] = useState(null);
   const [loading, setLoading] = useState(true);
 
-    useEffect(() => {
-        async function fetchChallenges() {
-            const res = await fetch('/api/challenges');
-            const data = await res.json();
-            setChallenges(data);
-            setLoading(false);
-        }
-        fetchChallenges();
-    }, []);
+  useEffect(() => {
+    async function fetchChallenges() {
+      const res = await fetch("/api/challenges");
+      const data = await res.json();
+      setChallenges(data);
+      setLoading(false);
+    }
+    fetchChallenges();
+  }, []);
 
-    const handleSelectChallenge = (challenge) => {
-        setSelectedChallenge(challenge);
-    };
+  const handleSelectChallenge = (challenge) => {
+    setSelectedChallenge(challenge);
+  };
 
   const handleNextChallenge = () => {
     const currentIndex = challenges.findIndex(
@@ -36,7 +35,8 @@ const Dashboard = () => {
     const currentIndex = challenges.findIndex(
       (challenge) => challenge === selectedChallenge
     );
-    const prevIndex = (currentIndex - 1 + challenges.length) % challenges.length;
+    const prevIndex =
+      (currentIndex - 1 + challenges.length) % challenges.length;
     setSelectedChallenge(challenges[prevIndex]);
   };
   if (loading) {
@@ -58,45 +58,43 @@ const Dashboard = () => {
       <div className="flex-1 lg:overflow-y-auto mt-8 mr-12 lg:pb-12 w-[70%] ml-8">
         <p className="text-2xl pb-4 font-bold">Challenge Details</p>
         <div className="flex flex-col space-y-2">
-          { 
-            selectedChallenge ? (
-              <>
-                <h2 className="text-lg pb-1">{selectedChallenge.title}</h2>
-                <p className="pt-2">{selectedChallenge.description}</p>
-                <h3>Example Solutions</h3>
-                {selectedChallenge.example_solutions ? (
-                  selectedChallenge.example_solutions.map((solution, index) => (
-                    <div key={index}>
-                      <h4>{solution.language}</h4>
-                      <pre className="bg-green-800 p-2 rounded-md text-white font-mono">
-                        {solution.code}
-                      </pre>
-                    </div>
-                  ))
-                ) : (
-                  <p>No example solutions</p>
-                )}
-              </>
-            ) : (
-              <>
-                <h2 className="text-lg pb-1">{challenges[0].title}</h2>
-                <p className="pt-2">{challenges[0].description}</p>
-                <h3>Example Solutions</h3>
-                {challenges[0].example_solutions ? (
-                  challenges[0].example_solutions.map((solution, index) => (
-                    <div key={index}>
-                      <h4>{solution.language}</h4>
-                      <pre className="bg-green-800 p-2 rounded-md text-white font-mono">
-                        {solution.code}
-                      </pre>
-                    </div>
-                  ))
-                ) : (
-                  <p>No example solutions</p>
-                )}
-              </>
-            )
-          }
+          {selectedChallenge ? (
+            <>
+              <h2 className="text-lg pb-1">{selectedChallenge.title}</h2>
+              <p className="pt-2">{selectedChallenge.description}</p>
+              <h3>Example Solutions</h3>
+              {selectedChallenge.example_solutions ? (
+                selectedChallenge.example_solutions.map((solution, index) => (
+                  <div key={index}>
+                    <h4>{solution.language}</h4>
+                    <pre className="bg-green-800 p-2 rounded-md text-white font-mono">
+                      {solution.code}
+                    </pre>
+                  </div>
+                ))
+              ) : (
+                <p>No example solutions</p>
+              )}
+            </>
+          ) : (
+            <>
+              <h2 className="text-lg pb-1">{challenges[0].title}</h2>
+              <p className="pt-2">{challenges[0].description}</p>
+              <h3>Example Solutions</h3>
+              {challenges[0].example_solutions ? (
+                challenges[0].example_solutions.map((solution, index) => (
+                  <div key={index}>
+                    <h4>{solution.language}</h4>
+                    <pre className="bg-green-800 p-2 rounded-md text-white font-mono">
+                      {solution.code}
+                    </pre>
+                  </div>
+                ))
+              ) : (
+                <p>No example solutions</p>
+              )}
+            </>
+          )}
         </div>
         <div className="flex items-center w-full justify-between mt-6 lg:hidden">
           <Button
