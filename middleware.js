b117872d352208688bd99fa56f10c2c70c5dd1f2
marksikaundi <emmanuelmark117@gmail.com>
2024-08-02 00:57:49 +0200

    created middlewares configs

---
+++ middleware.js
@@ -0,0 +1,21 @@
+import {
+  clerkMiddleware,
+  createRouteMatcher
+} from '@clerk/nextjs/server';
+
+const isProtectedRoute = createRouteMatcher([
+  '/dashboard(.*)',
+  '/academy(.*)',
+  '/profile(.*)',
+  '/account-profile(.*)',
+  '/settings(.*)',
+  '/challenges(.*)',
+]);
+
+export default clerkMiddleware((auth, req) => {
+  if (isProtectedRoute(req)) auth().protect();
+});
+
+export const config = {
+  matcher: ['/((?!.*\\..*|_next).*)', '/', '/(api|trpc)(.*)'],
+};
