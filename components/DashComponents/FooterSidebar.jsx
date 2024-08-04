68647cebd480ec03226abebcee4e19eba3f9d133
marksikaundi <emmanuelmark117@gmail.com>
2024-08-04 08:55:55 +0200

    chore: Update FooterSidebar component styling

--- components/DashComponents/FooterSidebar.jsx
+++ components/DashComponents/FooterSidebar.jsx
@@ -6,7 +6,7 @@ export default function FooterSidebar() {
   const currentYear = new Date().getFullYear();
 
   return (
-    <div className="">
+    <div className="pt-36">
       <div className="w-full px-3">
         <div className="border p-3 rounded-lg mb-4">
           <h2 className="text-lg font-bold py-2">Upgrade to pro</h2>
@@ -19,7 +19,7 @@ export default function FooterSidebar() {
         </div>
       </div>
       <Separator className="my-4 " />
-      <div className="flex space-x-4 text-xs justify-between items-center w-full px-3">
+      <div className="flex space-x-4 text-xs justify-between items-center px-3">
         <span>&#169; {currentYear} Lupleg Labs </span>
 
         <button className="hover:bg-gray-200 p-2 rounded-full">
